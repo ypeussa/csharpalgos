e95154246e9004fe700e1319e0e807b5fed8582f
Yrjö Peussa <peussa@iki.fi>
2017-11-09 16:22:18 +0200

    graphs started

---
+++ csharpalgos-unity/Assets/graph.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Graph<T> {
+    public List<GraphNode<T>> nodes;
+}
+
+public class GraphNode<T> {
+    public T data;
+    public List<GraphNode<T>> edges;
+    public List<float> weights;
+}
